a45258cb4069ab367fd6a2864aa2f9e556d99ee0
山崎浩毅 <yamazakihiroki@yamazakihiroshitakeshi-no-MacBook-Pro.local>
2019-05-18 01:34:05 +0900

    create AddPersonViewController

---
+++ PhotoFolderApp/Controllers/AddPersonViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AddPersonViewController.swift
+//  PhotoFolderApp
+//
+//  Created by 山崎浩毅 on 2019/05/18.
+//  Copyright © 2019 山崎浩毅. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+final class AddPersonViewController: UIViewController {
+    
+}
